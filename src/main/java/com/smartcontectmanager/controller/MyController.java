3593a483069e30e6b85cb475c3a1977c46f8e8ca
Ankur Singh <ankurrana231002@gmail.com>
2023-05-01 12:43:17 +0530

    Setting up BootStrap and Templet

--- src/main/java/com/smartcontectmanager/controller/MyController.java
+++ src/main/java/com/smartcontectmanager/controller/MyController.java
@@ -4,9 +4,11 @@ import com.smartcontectmanager.entity.Contact;
 import com.smartcontectmanager.entity.User;
 import com.smartcontectmanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MyController {
@@ -26,6 +28,22 @@ public class MyController {
 //        return "working ";
 //
 //    }
+
+    @GetMapping("/home")
+    public ModelAndView homePage(Model model){
+        model.addAttribute("title","Home - Smart Contact Manager");
+        ModelAndView modelAndView= new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
+    }
+
+    @GetMapping("/about")
+    public ModelAndView aboutPage(Model model){
+        model.addAttribute("title","About - Smart Contact Manager");
+        ModelAndView modelAndView= new ModelAndView();
+        modelAndView.setViewName("about");
+        return modelAndView;
+    }
 
 
 }
